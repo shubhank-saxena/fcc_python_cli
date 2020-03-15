f23b78d575e69e4cf79ad301bacb27639d64d1a8
shubhank-saxena <saxena.shubhank.19@gmail.com>
2020-03-15 13:29:47 +0530

    added cli options

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fcc_python_cli
 CLI tool for Python Learning
+
+To use -
+```git clone https://github.com/shubhank-saxena/fcc_python_cli.git```
+```cd fcc_python_cli```
+```pip3 install PyInquirer```
+```python3 script.py```
---
+++ script.py
@@ -0,0 +1,43 @@
+from pprint import pprint
+
+from PyInquirer import style_from_dict, Token, prompt
+from PyInquirer import Validator, ValidationError
+
+from examples import custom_style_3
+
+print("Hi, welcome to Python Class")
+print("Select an exercise and hit Enter to begin")
+
+modules = [
+    {
+        'type': 'list',
+        'name': 'option_selected',
+        'message': "",
+        'choices': ['Hello World',
+                    'Variable and Data Types', 
+                    'Working with Numbers', 
+                    'Getting input from users', 
+                    'Lists', 
+                    'Tuples',
+                    #'Functions,
+                    'If Else',
+                    'Dictionary', 
+                    'Loops', 
+                    'Nested Lists',
+                    'Try/Except', 
+                    'Reading/Writing Files', 
+                    'Modules and pip', 
+                    'Classes and Objects', 
+                    'Inheritence',
+                    "\n",
+                    'HELP',
+                    'CHOOSE LANGUAGE',
+                    'CREDITS',
+                    'CHECK FOR UPDATE',
+                    'EXIT'],
+        'filter': lambda val: val.lower()
+    }
+]
+
+module_selection = prompt(modules, style=custom_style_3)
+pprint(module_selection)
